aad5d7fdc4a336e9303cf3c2b3d52c59c4cf0a2f
joseavilasg <joseavilasg.dev@gmail.com>
2024-05-05 05:59:20 -0500

    Implement new cache

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { type Context, Hono } from "hono"
+import { HTTPException } from "hono/http-exception"
+import mime from "mime"
+
 export interface Env {
 	RESIZER_URL: string
-	RESIZER_TOKEN: string
 }
+
 export interface Caches {
 	default: {
 		put(request: Request | string, response: Response): Promise<undefined>
@@ -12,71 +16,85 @@ export interface Caches {
 
 declare let caches: Caches
 
-const handler: ExportedHandler<Env> = {
-	async fetch(request, env, ctx) {
-		const url = new URL(request.url)
-		console.log({ url })
+async function cacheResponse(ctx: ExecutionContext, url: string, headers: Record<string, string>) {
+	const cacheKey = new URL(url)
+	cacheKey.hostname = "cache.cdn.com"
+	const cache = caches.default
+	let response = await cache.match(cacheKey.href)
+	if (!response) {
+		response = await fetch(url, { headers })
+		if (response.ok) {
+			const res = new Response(response.body, {
+				headers: {
+					"Cache-Control": "public, max-age=604800, immutable",
+					"Content-Type":
+						response.headers.get("Content-Type") || "application/octet-stream",
+					"Content-Disposition": "inline",
+				},
+			})
+			ctx.waitUntil(cache.put(cacheKey.href, res.clone()))
+			response = res
+		} else {
+			response = new Response(response.body, { status: response.status })
+		}
+	}
+
+	return response
+}
+
+export class AssetService {
+	public static async getAsset(
+		ctx: Context,
+		params: Record<string, string>
+	): Promise<Response> {
+		const url = new URL(ctx.req.url)
 
 		if (url.pathname === "" || url.pathname === "/") {
 			return new Response("hello from server")
 		}
-
 		const filename = url.pathname.split("/").pop()
 		if (filename === "favicon.ico") {
-			return new Response("favicon not found", { status: 404 })
+			throw new HTTPException(404, {})
+		}
+		const { type, ...rest } = params
+		const newUrl = new URL("https://example.com");
+		const parts = url.pathname.split("/")
+		if (parts.length > 1 && parts[1].includes("http")) {
+			parts.shift()
+			parts.shift()
+		}
+		newUrl.host = parts[1]
+		newUrl.pathname = parts.slice(2).join("/")
+		for (const [key, value] of Object.entries(rest)) {
+			newUrl.searchParams.set(key, value)
 		}
 
-		// Construct the cache key from the cache URL
-		const cacheKey = new URL(url)
-		cacheKey.hostname = "cache.cdn.com"
-		const cache = caches.default
-		// const cache = await caches.open("resizer")
-
-		// Check whether the value is already available in the cache
-		// if not, you will need to fetch it from origin, and store it in the cache
-		let response = await cache.match(cacheKey.href)
-
-		if (!response) {
-			console.log(
-				`Response for request url: ${request.url} not present in cache. Fetching and caching request.`
-			)
-
-			const newUrl = new URL(env.RESIZER_URL)
-			newUrl.pathname = url.pathname
-			newUrl.search = url.search
-
-			const resizerToken = env.RESIZER_TOKEN
-			let headers = {}
+		let mediaType = mime.getType(filename!) || ""
+		let headers = {}
+		if (mediaType.startsWith("image") || type === "image") {
+			const oldHost = newUrl.host
+			newUrl.host = ctx.env.RESIZER_HOST
+			newUrl.pathname = `${oldHost}${newUrl.pathname}`
+			const resizerToken = ctx.env.RESIZER_TOKEN
 			if (resizerToken) {
 				headers = {
 					Authorization: `Bearer ${resizerToken}`,
+					"Content-Type": mediaType,
 				}
 			}
-
-			// If not in cache, get it from origin
-			response = await fetch(newUrl.href, {
-				headers
-			})
-
-			if (response.ok) {
-				const res = new Response(response.body, response)
-				res.headers.set("Cache-Control", "public, max-age=604800, immutable")
-				res.headers.set(
-					"Content-Type",
-					response.headers.get("Content-Type") || "application/octet-stream"
-				)
-				res.headers.set("Content-Disposition", "inline")
-
-				ctx.waitUntil(cache.put(cacheKey.href, res.clone()))
-				response = res
-			} else {
-				response = new Response(response.body, { status: response.status })
-			}
-		} else {
-			console.log(`Cache hit for: ${request.url}.`)
 		}
-		return response
-	},
+		newUrl.searchParams.sort()
+		const response = await cacheResponse(ctx.executionCtx, newUrl.href, headers)
+		return new Response(response.body, response)
+	}
 }
 
-export default handler
+const app = new Hono({ strict: false })
+app.get("*", async (c) => {
+	const url = new URL(c.req.url)
+	const params = Object.fromEntries(url.searchParams.entries())
+	const response = await AssetService.getAsset(c, params)
+	return response
+})
+
+export default app
